b889d424f6af3fc34a60d344307ad690267ef934
breno mazzini <mazzinibreno@gmail.com>
2024-04-27 14:05:14 -0300

    Update fazer-denuncia.blade.php

--- denuncia-anonima/resources/views/usuario/fazer-denuncia.blade.php
+++ denuncia-anonima/resources/views/usuario/fazer-denuncia.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Criar Usuário</title>
+    <title>Fazer denúncia</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
@@ -65,6 +65,19 @@
 
 <body>
     <div class="cabecalho">
+
+        <div class="">
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button class="rounded border border-danger bg-danger text-white" > <strong>Log out</strong> </button>
+                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                          this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
+        </div>
+
         <div>
             <h3 class="text-white">Nova denúncia</h3>
         </div>
