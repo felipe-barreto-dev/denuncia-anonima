2d71371a10b2759b53caa959c8686a95f13fa7fa
Rafael Ramiro <93105946+RMRafael@users.noreply.github.com>
2024-04-28 01:31:20 -0300

    Revert "Merge branch 'dev' into feat-page_protocol"
    This reverts commit 8892d15b56cfdb4e902943552822c07e08a744b5, reversing
    changes made to 57352e846684796f890da9d47cd2e38a612b2983.

--- denuncia-anonima/app/Http/Controllers/DenunciaController.php
+++ denuncia-anonima/app/Http/Controllers/DenunciaController.php
@@ -5,37 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Denuncia;
 use Illuminate\Http\Request;
-use App\Models\TipoDenuncia; // Certifique-se de importar o modelo de TipoDenuncia
 
 class DenunciaController extends Controller
 {
-
     public function create()
     {
-        $tiposDenuncia = TipoDenuncia::all();
-        return view('usuario.fazer-denuncia', ['tiposDenuncia' => $tiposDenuncia]);
-    }
-
-    public function generate_protocol()
-    {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        $characters_length = strlen($characters);
-
-        $protocol = '';
-
-        for ($i = 0; $i < 6; $i++) {
-            $random_index = rand(0, $characters_length - 1);
-            $protocol .= $characters[$random_index];
-        }
-
-        return $protocol;
+        return view('usuario.fazer-denuncia');
     }
 
     public function store(Request $request)
     {
         $denuncia = new Denuncia();
-        $denuncia->protocolo = $this->generate_protocol();
+        $denuncia->protocolo = 'protocolo';
         $denuncia->descricao = $request->input('descricao');
         $denuncia->titulo = $request->input('titulo');
         $denuncia->pessoas_afetadas = 'outros';
