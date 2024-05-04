28eade7494161d2306eba95fc95d640c71e35384
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-05-04 11:10:20 -0300

    feat: get user reports

--- denuncia-anonima/app/Http/Controllers/ShowReportsController.php
+++ denuncia-anonima/app/Http/Controllers/ShowReportsController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Denuncia;
 
 class ShowReportsController extends Controller
 {
     public function index()
     {
-        return view('site.index');
+        // Recupere todas as denúncias do usuário atual (você precisará de lógica para determinar o usuário atual)
+        $userReports = Denuncia::where('id_usuario', auth()->user()->id)->get();
+
+        // Passe os dados das denúncias para a visualização
+        return view('site.index', ['userReports' => $userReports]);
     }
 }
