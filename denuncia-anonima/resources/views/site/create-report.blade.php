333890320e2fd8106112e12dd5d9283dbd059521
Gabrielle Bussi <gabrielle.olibussi@gmail.com>
2024-05-04 17:01:54 -0300

    Mudanças de estrutura - melhores práticas de codificação

--- denuncia-anonima/resources/views/site/create-report.blade.php
+++ denuncia-anonima/resources/views/site/create-report.blade.php
@@ -1,70 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fazer denúncia</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
+@extends('head')
 
-    <style>
-        .cabecalho {
-            justify-content: space-between;
-            position: relative;
-            display: flex;
-            align-items: center;
-            padding: 20px;
-            background-color: #17A2B8;
-            height: 10vh;
-        }
+@section('title', 'Fazer Denúncia')
 
-        .cabecalho img {
-            width: 80px;
-        }
+@section('content')
 
-        .container-conteudo {
-            width: 100%;
-            display: flex;
-            flex-direction: row;
-        }
-
-        .container-informacoes {
-            width: 50%;
-            padding: 15px;
-        }
-
-        .container-formulario {
-            width: 50%;
-            padding: 15px;
-        }
-
-        .imagem-bem-estar img {
-            border-radius: 20px;
-        }
-
-        .bg-purple {
-            background-color: #738DED;
-        }
-
-        .stepper {
-            .line {
-                width: 2px;
-                background-color: lightgrey !important;
-            }
-
-            .lead {
-                font-size: 1.1rem;
-            }
-
-        }
-    </style>
-</head>
-
-<body>
     <div class="cabecalho">
         <div>
             <form method="POST" action="{{ route('logout') }}">
@@ -287,6 +227,4 @@
         </div>
 
     </div>
-</body>
-
-</html>
+@endsection
