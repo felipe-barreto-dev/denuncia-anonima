d539bedeb7b35e8ff02c99648964aed8d8da9b28
Rafael Ramiro <93105946+RMRafael@users.noreply.github.com>
2024-12-12 00:00:49 -0300

    UserController update
    Foi adicionado um filtro para exibir apenas os admins e analistas no painel de criação de usuário

--- denuncia-anonima/app/Http/Controllers/UserController.php
+++ denuncia-anonima/app/Http/Controllers/UserController.php
@@ -11,8 +11,8 @@ class UserController extends Controller
     public function create()
     {
         $perfis = Perfil::all();
-        $usuarios = Usuario::with('perfil')->get(); // Usuários existentes
-        return view('auth.register', ['perfis' => $perfis, 'usuarios' => $usuarios]);
+        $usuarios = Usuario::with('perfil')->whereIn('id_perfil', [1, 3])->get();
+            return view('auth.register', ['perfis' => $perfis, 'usuarios' => $usuarios]);
     }
 
     public function store(Request $request)
